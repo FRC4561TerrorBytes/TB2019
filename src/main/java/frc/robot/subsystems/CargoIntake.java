c9a3c91c9416abff8e3f6b32a70e16bc28bcd391
Lucas Therrien <ltherrien19@rthighschool.org>
2019-01-19 15:16:11 -0500

    LT - Untested:
    Skeletal Code for the cargo intake

---
+++ src/main/java/frc/robot/subsystems/CargoIntake.java
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Add your docs here.
+ */
+public class CargoIntake extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  private WPI_TalonSRX cargoIntakeLeftMotor;
+	private WPI_TalonSRX cargoIntakeRightMotor; 
+
+  public CargoIntake(){
+    this.cargoIntakeLeftMotor = new WPI_TalonSRX((Integer) null); //TODO: find real values
+    this.cargoIntakeRightMotor = new WPI_TalonSRX((Integer) null);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+  }
+
+  public void intakeCargo(){
+    cargoIntakeLeftMotor.set(1);
+    cargoIntakeRightMotor.set(1);
+  }
+
+  public void outtakeCargo(){
+    cargoIntakeLeftMotor.set(-1);
+    cargoIntakeRightMotor.set(-1);
+  }
+
+  public void stop(){
+    cargoIntakeLeftMotor.set(0);
+    cargoIntakeRightMotor.set(0);
+  }
+}
